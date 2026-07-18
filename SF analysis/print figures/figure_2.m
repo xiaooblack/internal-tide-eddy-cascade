b1cc345c89e683412ce997ff6009ef1b77faa3d8
Boyan Wu <133024174+xiaooblack@users.noreply.github.com>
2026-07-18 18:25:08 +0800

    Update combined Figure 2 script for GRL

--- SF analysis/print figures/figure_2.m
+++ SF analysis/print figures/figure_2.m
@@ -1,99 +1,74 @@
-    clear
-    cd 'C:\Users\Lenovo\Desktop\结构函数'
-    warning off all;  
-    %read grid
-    rmpath('E:\Roms_CROCO_source\croco_tools-v1.3\croco_tools-v1.3\UTILITIES\mask');% 使用原ncread函数
-    x_rho=ncread('I:\NCS_wu_0.5km_2day_filt\NCS_wu_0.5km_15m_notide_filt.nc','x_rho');
-    y_rho=ncread('I:\NCS_wu_0.5km_2day_filt\NCS_wu_0.5km_15m_notide_filt.nc','y_rho');
-    load('I:\NCS_wu_0.5km_2day_filt\lonlat_rho_origin.mat')
-    u_lp_tide=ncread('I:\NCS_wu_0.5km_2day_filt\NCS_wu_0.5km_15m_tide_filt_order4.nc','u_lp',[1 1 590],[inf inf 1]);
-    v_lp_tide=ncread('I:\NCS_wu_0.5km_2day_filt\NCS_wu_0.5km_15m_tide_filt_order4.nc','v_lp',[1 1 590],[inf inf 1]);
-    
-    u_hp_tide=ncread('I:\NCS_wu_0.5km_2day_filt\NCS_wu_0.5km_15m_tide_filt_order4.nc','u_hp',[1 1 590],[inf inf 1]);
-    v_hp_tide=ncread('I:\NCS_wu_0.5km_2day_filt\NCS_wu_0.5km_15m_tide_filt_order4.nc','v_hp',[1 1 590],[inf inf 1]);
-    
-    u_lp_notide=ncread('I:\NCS_wu_0.5km_2day_filt\NCS_wu_0.5km_15m_notide_filt_order4.nc','u_lp',[1 1 590],[inf inf 1]);
-    v_lp_notide=ncread('I:\NCS_wu_0.5km_2day_filt\NCS_wu_0.5km_15m_notide_filt_order4.nc','v_lp',[1 1 590],[inf inf 1]);
-    
-    u_hp_notide=ncread('I:\NCS_wu_0.5km_2day_filt\NCS_wu_0.5km_15m_notide_filt_order4.nc','u_hp',[1 1 590],[inf inf 1]);
-    v_hp_notide=ncread('I:\NCS_wu_0.5km_2day_filt\NCS_wu_0.5km_15m_notide_filt_order4.nc','v_hp',[1 1 590],[inf inf 1]);
-    
-    [dudx,dudy]=model_gradient(x_rho,y_rho,u_lp_tide);
-    [dvdx,dvdy]=model_gradient(x_rho,y_rho,v_lp_tide);
-    zeta_lp_tide=(dvdx-dudy)./sw_f(18);
-    div_lp_tide=(dudx+dvdy)./sw_f(18);
-    
-    [dudx,dudy]=model_gradient(x_rho,y_rho,u_lp_notide);
-    [dvdx,dvdy]=model_gradient(x_rho,y_rho,v_lp_notide);
-    zeta_lp_notide=(dvdx-dudy)./sw_f(18);
-    div_lp_notide=(dudx+dvdy)./sw_f(18);
-    
-    zeta_lp_tide(zeta_lp_tide>10)=nan;
-    zeta_lp_tide(zeta_lp_tide<-10)=nan;
-    zeta_lp_tide=inpaint_nans(zeta_lp_tide,1);
-    %%% SF3 & F(k)
-    % ===================== 1. 核心可调参数 =====================
-    % --- 数据参与拟合的范围（尽量用全部数据）---
-    r_min_data = 1;     % km  所有 >= 1 km 的数据都参与拟合
-    r_max_data = 500;   % km  所有 <= 100 km 的数据都参与拟合
-    
-    % --- 你关心的物理波数范围（对应目标尺度 5~90 km）---
-    r_min_target = 1;   % km  → k_max = 1/5 = 0.2  1/km
-    r_max_target = 99;  % km  → k_min = 1/90 ≈ 0.011 1/km
-    
-    smooth_win  = 8;
-    lambda_reg  = 1e-2;
-    Nk          = 100;
-    
-    % ===================== 2. 读取 & 预处理数据 =====================
-    r = ncread('I:/NCS_wu_0.5km_15layer_all/SF_NCS_wu_0.5km_15layers_tide_filt_order4_all.nc','r');
-    sum_D3_tide_lp = squeeze(ncread('I:/NCS_wu_0.5km_15layer_all/SF_NCS_wu_0.5km_15layers_tide_filt_order4_all.nc', ...
-        'sum_D3_lp', [1 14 49], [inf 1 599]));
-    sum_DLL_tide_lp = squeeze(ncread('I:/NCS_wu_0.5km_15layer_all/SF_NCS_wu_0.5km_15layers_tide_filt_order4_all.nc', ...
-        'sum_DLL_lp', [1 14 49], [inf 1 599]));
-    sum_DTT_tide_lp = squeeze(ncread('I:/NCS_wu_0.5km_15layer_all/SF_NCS_wu_0.5km_15layers_tide_filt_order4_all.nc', ...
-        'sum_DTT_lp', [1 14 49], [inf 1 599]));
-    sum_D3_zeta_tide_lp = squeeze(ncread('I:/NCS_wu_0.5km_15layer_all/SF_NCS_wu_0.5km_15layers_tide_filt_order4_all.nc', ...
-        'sum_D3_zeta_lp', [1 14 49], [inf 1 599]));
-    sum_D2_zeta_tide_lp = squeeze(ncread('I:/NCS_wu_0.5km_15layer_all/SF_NCS_wu_0.5km_15layers_tide_filt_order4_all.nc', ...
-        'sum_D2_zeta_lp', [1 14 49], [inf 1 599]));
-    counts_tide_lp = squeeze(ncread('I:/NCS_wu_0.5km_15layer_all/SF_NCS_wu_0.5km_15layers_tide_filt_order4_all.nc', ...
-        'counts_lp', [1 14 49], [inf 1 599]));
-    
-    S3_tide_lp = sum(sum_D3_tide_lp, 2) ./ sum(counts_tide_lp, 2);
-    S3_zeta_tide_lp = sum(sum_D3_zeta_tide_lp, 2) ./ sum(counts_tide_lp, 2);
-    S2_zeta_tide_lp = sum(sum_D2_zeta_tide_lp, 2) ./ sum(counts_tide_lp, 2);
-    S2_tide_lp=sum(sum_DLL_tide_lp, 2) ./ sum(counts_tide_lp, 2)+sum(sum_DTT_tide_lp, 2) ./ sum(counts_tide_lp, 2);
-    [k_tide_lp,R_tide_lp,F_k_tide_lp,Pidk_tide_lp,S3_recon_tide_lp]=calc_epsilon_Fk(r,S3_tide_lp,r_min_data,r_max_data,r_min_target,r_max_target,smooth_win,lambda_reg,Nk);
-    [k_zeta_tide_lp,R_zeta_tide_lp,F_k_zeta_tide_lp,Pidk_zeta_tide_lp,S3_zeta_recon_tide_lp]=calc_epsilon_Fk_zeta(r,S3_zeta_tide_lp,r_min_data,r_max_data,r_min_target,r_max_target,smooth_win,lambda_reg,Nk);
-    
-    r = ncread('I:/NCS_wu_0.5km_15layer_all/SF_NCS_wu_0.5km_15layers_notide_filt_order4_all.nc','r');
-    sum_D3_notide_lp = squeeze(ncread('I:/NCS_wu_0.5km_15layer_all/SF_NCS_wu_0.5km_15layers_notide_filt_order4_all.nc', ...
-        'sum_D3_lp', [1 14 49], [inf 1 599]));
-    sum_DLL_notide_lp = squeeze(ncread('I:/NCS_wu_0.5km_15layer_all/SF_NCS_wu_0.5km_15layers_notide_filt_order4_all.nc', ...
-        'sum_DLL_lp', [1 14 49], [inf 1 599]));
-    sum_DTT_notide_lp = squeeze(ncread('I:/NCS_wu_0.5km_15layer_all/SF_NCS_wu_0.5km_15layers_notide_filt_order4_all.nc', ...
-        'sum_DTT_lp', [1 14 49], [inf 1 599]));
-    sum_D3_zeta_notide_lp = squeeze(ncread('I:/NCS_wu_0.5km_15layer_all/SF_NCS_wu_0.5km_15layers_notide_filt_order4_all.nc', ...
-        'sum_D3_zeta_lp', [1 14 49], [inf 1 599]));
-    sum_D2_zeta_notide_lp = squeeze(ncread('I:/NCS_wu_0.5km_15layer_all/SF_NCS_wu_0.5km_15layers_notide_filt_order4_all.nc', ...
-        'sum_D2_zeta_lp', [1 14 49], [inf 1 599]));
-    counts_notide_lp = squeeze(ncread('I:/NCS_wu_0.5km_15layer_all/SF_NCS_wu_0.5km_15layers_notide_filt_order4_all.nc', ...
-        'counts_lp', [1 14 49], [inf 1 599]));
-    
-    S3_notide_lp = sum(sum_D3_notide_lp, 2) ./ sum(counts_notide_lp, 2);
-    S3_zeta_notide_lp = sum(sum_D3_zeta_notide_lp, 2) ./ sum(counts_notide_lp, 2);
-    S2_zeta_notide_lp = sum(sum_D2_zeta_notide_lp, 2) ./ sum(counts_notide_lp, 2);
-    S2_notide_lp=sum(sum_DLL_notide_lp, 2) ./ sum(counts_notide_lp, 2)+sum(sum_DTT_notide_lp, 2) ./ sum(counts_notide_lp, 2);
-    [k_notide_lp,R_notide_lp,F_k_notide_lp,Pidk_notide_lp,S3_recon_notide_lp]=calc_epsilon_Fk(r,S3_notide_lp,r_min_data,r_max_data,r_min_target,r_max_target,smooth_win,lambda_reg,Nk);
-    [k_zeta_notide_lp,R_zeta_notide_lp,F_k_zeta_notide_lp,Pidk_zeta_notide_lp,S3_zeta_recon_notide_lp]=calc_epsilon_Fk_zeta(r,S3_zeta_notide_lp,r_min_data,r_max_data,r_min_target,r_max_target,smooth_win,lambda_reg,Nk);
+clear
+cd 'C:\Users\Lenovo\Desktop\结构函数'
+warning off all;  
+%read grid
+rmpath('E:\Roms_CROCO_source\croco_tools-v1.3\croco_tools-v1.3\UTILITIES\mask');% 使用原ncread函数
+x_rho=ncread('I:\NCS_wu_0.5km_2day_filt\NCS_wu_0.5km_15m_notide_filt.nc','x_rho');
+y_rho=ncread('I:\NCS_wu_0.5km_2day_filt\NCS_wu_0.5km_15m_notide_filt.nc','y_rho');
+load('I:\NCS_wu_0.5km_2day_filt\lonlat_rho_origin.mat')
+u_lp_tide=ncread('I:\NCS_wu_0.5km_2day_filt\NCS_wu_0.5km_15m_tide_filt_order4.nc','u_lp',[1 1 590],[inf inf 1]);
+v_lp_tide=ncread('I:\NCS_wu_0.5km_2day_filt\NCS_wu_0.5km_15m_tide_filt_order4.nc','v_lp',[1 1 590],[inf inf 1]);
+
+u_hp_tide=ncread('I:\NCS_wu_0.5km_2day_filt\NCS_wu_0.5km_15m_tide_filt_order4.nc','u_hp',[1 1 590],[inf inf 1]);
+v_hp_tide=ncread('I:\NCS_wu_0.5km_2day_filt\NCS_wu_0.5km_15m_tide_filt_order4.nc','v_hp',[1 1 590],[inf inf 1]);
+
+u_lp_notide=ncread('I:\NCS_wu_0.5km_2day_filt\NCS_wu_0.5km_15m_notide_filt_order4.nc','u_lp',[1 1 590],[inf inf 1]);
+v_lp_notide=ncread('I:\NCS_wu_0.5km_2day_filt\NCS_wu_0.5km_15m_notide_filt_order4.nc','v_lp',[1 1 590],[inf inf 1]);
+
+u_hp_notide=ncread('I:\NCS_wu_0.5km_2day_filt\NCS_wu_0.5km_15m_notide_filt_order4.nc','u_hp',[1 1 590],[inf inf 1]);
+v_hp_notide=ncread('I:\NCS_wu_0.5km_2day_filt\NCS_wu_0.5km_15m_notide_filt_order4.nc','v_hp',[1 1 590],[inf inf 1]);
+
+[dudx,dudy]=model_gradient(x_rho,y_rho,u_lp_tide);
+[dvdx,dvdy]=model_gradient(x_rho,y_rho,v_lp_tide);
+zeta_lp_tide=(dvdx-dudy)./sw_f(18);
+div_lp_tide=(dudx+dvdy)./sw_f(18);
+
+[dudx,dudy]=model_gradient(x_rho,y_rho,u_lp_notide);
+[dvdx,dvdy]=model_gradient(x_rho,y_rho,v_lp_notide);
+zeta_lp_notide=(dvdx-dudy)./sw_f(18);
+div_lp_notide=(dudx+dvdy)./sw_f(18);
+
+zeta_lp_tide(zeta_lp_tide>10)=nan;
+zeta_lp_tide(zeta_lp_tide<-10)=nan;
+zeta_lp_tide=inpaint_nans(zeta_lp_tide,1);
+
+%%% SF3 & F(k)
+% ===================== 1. 核心可调参数 =====================
+% --- 数据参与拟合的范围（尽量用全部数据）---
+r_min_data = 1;     % km  所有 >= 1 km 的数据都参与拟合
+r_max_data = 500;   % km  所有 <= 100 km 的数据都参与拟合
+
+% --- 你关心的物理波数范围（对应目标尺度 5~90 km）---
+r_min_target = 1;   % km  → k_max = 1/5 = 0.2  1/km
+r_max_target = 99;  % km  → k_min = 1/90 ≈ 0.011 1/km
+
+smooth_win  = 5;
+lambda_reg  = 1e-2;
+Nk          = 100;
+
+% ===================== 2. 读取 & 预处理数据 =====================
+load('SF_lp_model_witherrobar.mat')
+
+[k_tide_lp,R_tide_lp,F_k_tide_lp,Pidk_tide_lp,S3_recon_tide_lp]=calc_epsilon_Fk(r,S3_tide_lp,r_min_data,r_max_data,r_min_target,r_max_target,smooth_win,lambda_reg,Nk);
+[k_zeta_tide_lp,R_zeta_tide_lp,F_k_zeta_tide_lp,Pidk_zeta_tide_lp,S3_zeta_recon_tide_lp]=calc_epsilon_Fk_zeta(r,S3_zeta_tide_lp,r_min_data,r_max_data,r_min_target,r_max_target,smooth_win,lambda_reg,Nk);
+[k_tide_lp_low,R_tide_lp_low,F_k_tide_lp_low,Pidk_tide_lp_low,S3_recon_tide_lp_low]=calc_epsilon_Fk(r,S3_tide_lp_low,r_min_data,r_max_data,r_min_target,r_max_target,smooth_win,lambda_reg,Nk);
+[k_tide_lp_high,R_tide_lp_high,F_k_tide_lp_high,Pidk_tide_lp_high,S3_recon_tide_lp_high]=calc_epsilon_Fk(r,S3_tide_lp_high,r_min_data,r_max_data,r_min_target,r_max_target,smooth_win,lambda_reg,Nk);
+[k_zeta_tide_lp_low,R_zeta_tide_lp_low,F_k_zeta_tide_lp_low,Pidk_zeta_tide_lp_low,S3_zeta_recon_tide_lp_low]=calc_epsilon_Fk_zeta(r,S3_zeta_tide_lp_low,r_min_data,r_max_data,r_min_target,r_max_target,smooth_win,lambda_reg,Nk);
+[k_zeta_tide_lp_high,R_zeta_tide_lp_high,F_k_zeta_tide_lp_high,Pidk_zeta_tide_lp_high,S3_zeta_recon_tide_lp_high]=calc_epsilon_Fk_zeta(r,S3_zeta_tide_lp_high,r_min_data,r_max_data,r_min_target,r_max_target,smooth_win,lambda_reg,Nk);
+
+[k_notide_lp,R_notide_lp,F_k_notide_lp,Pidk_notide_lp,S3_recon_notide_lp]=calc_epsilon_Fk(r,S3_notide_lp,r_min_data,r_max_data,r_min_target,r_max_target,smooth_win,lambda_reg,Nk);
+[k_zeta_notide_lp,R_zeta_notide_lp,F_k_zeta_notide_lp,Pidk_zeta_notide_lp,S3_zeta_recon_notide_lp]=calc_epsilon_Fk_zeta(r,S3_zeta_notide_lp,r_min_data,r_max_data,r_min_target,r_max_target,smooth_win,lambda_reg,Nk);
+[k_notide_lp_low,R_notide_lp_low,F_k_notide_lp_low,Pidk_notide_lp_low,S3_recon_notide_lp_low]=calc_epsilon_Fk(r,S3_notide_lp_low,r_min_data,r_max_data,r_min_target,r_max_target,smooth_win,lambda_reg,Nk);
+[k_notide_lp_high,R_notide_lp_high,F_k_notide_lp_high,Pidk_notide_lp_high,S3_recon_notide_lp_high]=calc_epsilon_Fk(r,S3_notide_lp_high,r_min_data,r_max_data,r_min_target,r_max_target,smooth_win,lambda_reg,Nk);
+[k_zeta_notide_lp_low,R_zeta_notide_lp_low,F_k_zeta_notide_lp_low,Pidk_zeta_notide_lp_low,S3_zeta_recon_notide_lp_low]=calc_epsilon_Fk_zeta(r,S3_zeta_notide_lp_low,r_min_data,r_max_data,r_min_target,r_max_target,smooth_win,lambda_reg,Nk);
+[k_zeta_notide_lp_high,R_zeta_notide_lp_high,F_k_zeta_notide_lp_high,Pidk_zeta_notide_lp_high,S3_zeta_recon_notide_lp_high]=calc_epsilon_Fk_zeta(r,S3_zeta_notide_lp_high,r_min_data,r_max_data,r_min_target,r_max_target,smooth_win,lambda_reg,Nk);
 
 %% Vorticity & Velocity SF2\SF3
 color_red=[229,76,94]/255;
 color_blue=[72,116,203]/255;
 figure
-set(gcf, 'unit', 'centimeters', 'position', [1 2 26 16]);% [left, bottom, width, height]
-AH1 = axes('Parent', gcf, 'unit', 'centimeters', 'Position', [4.5 8.5 7.5 6]);
+set(gcf, 'unit', 'centimeters', 'position', [1 1 26 23.5]);% [left, bottom, width, height]
+AH1 = axes('Parent', gcf, 'unit', 'centimeters', 'Position', [4.5 16.5 7.5 6]);
 pcolor(lon_rho,lat_rho,zeta_lp_tide);shading interp
 rectangle('Position', [116, 18, 1, 0.8], 'EdgeColor', [0, 0, 0], 'LineStyle', '--', 'LineWidth', 1.5);
 caxis([-1 1])
@@ -107,9 +82,9 @@ set(AH1,'fontsize',11,'fontname','Arial');
 set(AH1,'layer','top');
 set(AH1,'linewidth', 1)
 tit=title('\bf(a) \rm2-day Low-pass Ro (Tide)','fontsize',12,'fontname','Arial');
-set(tit, 'unit', 'centimeters','Position', [2.8 6 0])
-
-AH1b= axes('Parent', gcf, 'unit', 'centimeters', 'Position', [8.74 11.88 3.25 2.6]);
+set(tit, 'unit', 'centimeters','Position', [2.9 6.1 0])
+    
+AH1b= axes('Parent', gcf, 'unit', 'centimeters', 'Position', [8.74 19.88 3.25 2.6]);
 pcolor(lon_rho,lat_rho,zeta_lp_tide);shading flat;hold on
 contour(lon_rho,lat_rho,zeta_lp_tide,[0.2,0.2],'LineStyle','-','linecolor',[0.3 0.3 0.3],'linewidth',1);
 caxis([-1 1])
@@ -121,7 +96,7 @@ set(gca, 'LineWidth', 1.2);
 set(gca,'layer','top');set(gca,'ticklength',[0 0]);
 
 
-AH2 = axes('Parent', gcf, 'unit', 'centimeters', 'Position', [15 8.5 7.5 6]);
+AH2 = axes('Parent', gcf, 'unit', 'centimeters', 'Position', [15 16.5 7.5 6]);
 pcolor(lon_rho,lat_rho,zeta_lp_notide);shading flat
 rectangle('Position', [115.3, 18, 1, 0.8], 'EdgeColor', [0, 0, 0], 'LineStyle', '--', 'LineWidth', 1.5);
 caxis([-1 1])
@@ -135,12 +110,12 @@ set(AH2,'fontsize',11,'fontname','Arial');
 set(AH2,'linewidth', 1)
 set(AH2,'layer','top');
 hhh = colorbar;
-set(hhh, 'unit', 'centimeters','Position', [23.5 9.5 0.4 4]);
+set(hhh, 'unit', 'centimeters','Position', [23.5 17.5 0.4 4]);
 title(hhh,'\zeta/\itf');
 tit=title('\bf(b) \rm2-day Low-pass Ro (Notide)','fontsize',12,'fontname','Arial');
-set(tit, 'unit', 'centimeters','Position', [3.2 6 0])
+set(tit, 'unit', 'centimeters','Position', [3.1 6.1 0])
 
-AH2b= axes('Parent', gcf, 'unit', 'centimeters', 'Position', [19.25 11.88 3.25 2.6]);
+AH2b= axes('Parent', gcf, 'unit', 'centimeters', 'Position', [19.25 19.88 3.25 2.6]);
 pcolor(lon_rho,lat_rho,zeta_lp_notide);shading interp;hold on
 contour(lon_rho,lat_rho,zeta_lp_notide,[0.2,0.2],'LineStyle','-','linecolor',[0.3 0.3 0.3],'linewidth',1);
 caxis([-1 1])
@@ -151,7 +126,7 @@ set(gca,'xticklabel',[]);set(gca,'yticklabel',[]);box on
 set(gca, 'LineWidth', 1.2);
 set(gca,'layer','top');set(gca,'ticklength',[0 0]);
 
-AH3 = axes('Parent', gcf, 'unit', 'centimeters', 'Position', [2 2 6 4.8]);
+AH3 = axes('Parent', gcf, 'unit', 'centimeters', 'Position', [2 10 6 4.8]);
 plot(r, S2_zeta_tide_lp./r.*1e10,'-','color',color_red, 'LineWidth', 1.2, 'DisplayName', 'Tide lp'); hold on
 plot(r, S2_zeta_notide_lp./r.*1e10, '-','color',color_blue, 'LineWidth', 1.2, 'DisplayName', 'Notide lp'); 
 set(gca, 'XScale', 'log');
@@ -168,7 +143,7 @@ legend('show', 'Location', 'southwest','FontSize', 12);box on
 set(gca,'layer','top');
 
 
-AH6 = axes('Parent', gcf, 'unit', 'centimeters', 'Position', [10.5 2 6 4.8]);
+AH4 = axes('Parent', gcf, 'unit', 'centimeters', 'Position', [10.5 10 6 4.8]);
 loglog(r, S3_zeta_tide_lp,'-','color',color_red, 'LineWidth', 1.2, 'DisplayName', 'tide lp'); hold on
 loglog(r, -S3_zeta_tide_lp, '--','color',color_red, 'LineWidth', 1.2, 'DisplayName', 'tide lp'); 
 loglog(r, S3_zeta_notide_lp,'-','color',color_blue, 'LineWidth', 1.2, 'DisplayName', 'tide lp'); hold on
@@ -185,7 +160,7 @@ tit=title('\bf(d) \rmVorticity SF3','fontsize',12,'fontname','Arial');
 set(tit, 'unit', 'centimeters','Position', [1.55 4.9 0])
 set(gca,'layer','top');
 
-AH7 = axes('Parent', gcf, 'unit', 'centimeters', 'Position', [19 2 6 4.8]);
+AH5 = axes('Parent', gcf, 'unit', 'centimeters', 'Position', [19 10 6 4.8]);
 loglog(r, S3_tide_lp,'-','color',color_red, 'LineWidth', 1.2, 'DisplayName', 'tide lp'); hold on
 loglog(r, -S3_tide_lp, '--','color',color_red, 'LineWidth', 1.2, 'DisplayName', 'tide lp'); 
 loglog(r, S3_notide_lp,'-','color',color_blue, 'LineWidth', 1.2, 'DisplayName', 'tide lp'); hold on
@@ -202,4 +177,47 @@ tit=title('\bf(e) \rmVelocity SF3','fontsize',12,'fontname','Arial');
 set(tit, 'unit', 'centimeters','Position', [1.55 4.9 0])
 set(gca,'layer','top');
 
-print(gcf, 'D:\学花招的素材\初稿\wave-flow interation influence flow energy cascade\figure\Fig 2.tiff', '-dtiff', '-r600')   % TIFF, 600 dpi
+
+AH6 = axes('Parent', gcf, 'unit', 'centimeters', 'Position', [14.5 1.5 10 6]);
+plot(k_tide_lp, F_k_tide_lp.*1e6,'-','color',color_red, 'LineWidth', 1.2,'DisplayName', 'Tide lp'); hold on;
+fill([k_tide_lp  fliplr(k_tide_lp)], [F_k_tide_lp_high.*1e6 ; flipud(F_k_tide_lp_low.*1e6)], color_red, ...
+    'FaceAlpha', 0.3, 'EdgeColor', 'none','HandleVisibility','off');
+plot(k_notide_lp, F_k_notide_lp.*1e6,'-','color',color_blue, 'LineWidth', 1.2,'DisplayName', 'Notide lp'); hold on;
+fill([k_notide_lp  fliplr(k_notide_lp)], [F_k_notide_lp_high.*1e6 ; flipud(F_k_notide_lp_low.*1e6)], color_blue, ...
+    'FaceAlpha', 0.3, 'EdgeColor', 'none','HandleVisibility','off');
+yline(0, 'LineStyle','--','Color',[0.3 0.3 0.3], 'LineWidth', 1, 'HandleVisibility','off','Layer','bottom');hold on;
+xline(0.12, 'LineStyle','--','Color',[1 0 1], 'LineWidth', 1, 'HandleVisibility','off','Layer','bottom');hold on;
+text(0.055,-15.5,'8.3km','fontsize',15,'fontname','Arial','Color',[1 0 1]);
+text(0.0105,0.9,'Downscale transfer (+)','fontsize',11,'fontname','Arial','Color',[0.3 0.3 0.3]);
+text(0.0105,-0.8,'Upscale transfer (-)','fontsize',11,'fontname','Arial','Color',[0.3 0.3 0.3]);
+xlabel('\itk \rm(1/km)'); ylabel('F(\itk\rm) (m^3s^{-3}km^{-1} ×10^{-6})');
+set(gca, 'XScale', 'log');set(gca,'linewidth', 1)
+set(gca,'FontName', 'Arial','FontSize',12) 
+tit=title('\bf(g) \rmSpectral Energy flux','FontName', 'Arial','FontSize',13); 
+set(tit, 'unit', 'centimeters','Position', [2.5 6.1 0])
+ylim([-17 2])
+xlim([1e-2 1]);
+legend('show', 'Location', 'southeast','FontSize', 13);box on
+
+AH7 = axes('Parent', gcf, 'unit', 'centimeters', 'Position', [2 1.5 10 6]);
+plot(k_zeta_tide_lp, F_k_zeta_tide_lp.*1e14,'-','color',color_red, 'LineWidth', 1.2,'DisplayName', 'Tide lp'); hold on;
+fill([k_zeta_tide_lp  fliplr(k_zeta_tide_lp)], [F_k_zeta_tide_lp_high.*1e14 ; flipud(F_k_zeta_tide_lp_low.*1e14)], color_red, ...
+    'FaceAlpha', 0.3, 'EdgeColor', 'none','HandleVisibility','off');
+plot(k_zeta_notide_lp, F_k_zeta_notide_lp.*1e14,'-','color',color_blue, 'LineWidth', 1.2,'DisplayName', 'Notide lp'); hold on;
+fill([k_zeta_notide_lp  fliplr(k_zeta_notide_lp)], [F_k_zeta_notide_lp_high.*1e14 ; flipud(F_k_zeta_notide_lp_low.*1e14)], color_blue, ...
+    'FaceAlpha', 0.3, 'EdgeColor', 'none','HandleVisibility','off');
+yline(0, 'LineStyle','--','Color',[0.3 0.3 0.3], 'LineWidth', 1, 'HandleVisibility','off','Layer','bottom');hold on;
+yline(8.2, 'LineStyle','--','Color',color_red, 'LineWidth', 1, 'HandleVisibility','off','Layer','bottom');hold on;
+text(1.1e-2,8.7,'8.2×10^{-14}','fontsize',12,'fontname','Arial','Color',color_red);
+yline(7.2, 'LineStyle','--','Color',color_blue, 'LineWidth', 1, 'HandleVisibility','off','Layer','bottom');hold on;
+text(1.1e-2,6.7,'7.2×10^{-14}','fontsize',12,'fontname','Arial','Color',color_blue);
+xlabel('\itk \rm(1/km)','FontSize',13); ylabel('F_\omega(\itk\rm) (ms^{-3}km^{-1} ×10^{-14})','FontSize',13);
+set(gca, 'XScale', 'log');set(gca,'linewidth', 1)
+set(gca,'FontName', 'Arial','FontSize',12)
+tit=title('\bf(f) \rmSpectral Enstrophy flux','FontName', 'Arial','FontSize',13); 
+set(tit, 'unit', 'centimeters','Position', [2.85 6.1 0])
+ylim([-0.5 10])
+xlim([1e-2 1]);
+legend('show', 'Location', 'southeast','FontSize', 13);box on
+
+print(gcf, 'D:\学花招的素材\初稿\wave-flow interation influence flow energy cascade\figure\Fig 2 GRL.tiff', '-dtiff', '-r600')   % TIFF, 600 dpi
